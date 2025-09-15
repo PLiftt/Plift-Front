4d5c9c0c75b52263533461c69efbf42f56f380f1
danaevenegas <danaevenegasmaldonado@gmail.com>
2025-09-15 18:59:21 -0300

    agregar servicio auth

--- services/authService.ts
+++ services/authService.ts
@@ -1,17 +1,33 @@
 import axios from "axios";
+import { saveToken, deleteToken, getToken } from "./secureStore";
 
-const API_URL = "http://127.0.0.1:8000"; 
+const API_URL = "http://192.168.1.88:8000"; 
 
 export const registerUser = async (data: any) => {
-  const response = await axios.post(`${API_URL}/register/`, data);
-  console.log(response)
-  return response.data;
+  try {
+    const response = await axios.post(`${API_URL}/register/`, data);
+    console.log("Respuesta del servidor:", response.data);
+
+    // Guardamos solo si existen
+    if (response.data.access) await saveToken("accessToken", response.data.access);
+    if (response.data.refresh) await saveToken("refreshToken", response.data.refresh);
+
+    return response.data;
+  } catch (error: any) {
+    console.error("Error en registerUser:", error.response?.data || error.message);
+    throw error;
+  }
 };
 
 export const loginUser = async (data: { email: string; password: string }) => {
   try {
-    console.log(data)
     const response = await axios.post(`${API_URL}/token/`, data);
+    console.log("Respuesta login:", response.data);
+
+    // Guardamos tokens en SecureStore
+    if (response.data.access) await saveToken("accessToken", response.data.access);
+    if (response.data.refresh) await saveToken("refreshToken", response.data.refresh);
+
     return response.data;
   } catch (error: any) {
     console.error("Error en loginUser:", error.response?.data || error.message);
